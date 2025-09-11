0f6b8678e60b63ab87a8702b6e71ab9929c6950e
mohadeev <skendoulmohamed@gmail.com>
2025-09-11 12:13:42 +0200

    save

--- src/app/api/twilio/voice/route.ts
+++ src/app/api/twilio/voice/route.ts
@@ -2,10 +2,13 @@ import { NextResponse } from 'next/server'
 import twilio from 'twilio'
 
 export async function POST(req: Request) {
+	// TwiML Response
 	const twiml = new twilio.twiml.VoiceResponse()
 
-	// Call Twilio number, then forward to personal number
+	// Dial your Twilio phone first
 	const dial = twiml.dial({ callerId: process.env.TWILIO_PHONE_NUMBER })
+
+	// Forward to your personal number
 	dial.number('+34614520461')
 
 	return new NextResponse(twiml.toString(), {
